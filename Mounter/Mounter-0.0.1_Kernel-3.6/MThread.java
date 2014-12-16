3bf90dfffef494163bb5f0c806abdbfeba906608
swap <swap@swap.(none)>
2014-12-16 21:02:02 +0530

    RMC@swap

---
+++ Mounter/Mounter-0.0.1_Kernel-3.6/MThread.java
@@ -0,0 +1,15 @@
+/*Application Name: Mounter
+Version: 0.0.1(Beta version)
+Developer&Maintainer: Swapnil J. Udapure
+Repository: https://github.com/swapgit/swap-repo
+E-mail: swapnil.udapure5@gmail.com*/
+
+/*   */ class MThread
+/*   */ {
+/*   */   public static void main(String[] paramArrayOfString)
+/*   */     throws Exception
+/*   */   {
+/* 6 */     new JNIConnection();
+/*   */   }
+/*   */ }
+
